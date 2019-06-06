e23913d8e365f55fd68647cd8cfede40d2e26e45
nelsong27 <nelson.gomez@puppet.com>
2019-06-06 14:00:22 -0400

    Add files via upload

---
+++ tasks/timesync.ps1
@@ -0,0 +1,19 @@
+# Puppet Task Name: timesync
+#
+# This is where you put the shell code for your task.
+#
+# You can write Puppet tasks in any language you want and it's easy to
+# adapt an existing Python, PowerShell, Ruby, etc. script. Learn more at:
+# https://puppet.com/docs/bolt/0.x/writing_tasks.html
+#
+echo "Reconfiguring W32Time..."
+w32tm /config /syncfromflags:MANUAL /manualpeerlist:"0.nl.pool.ntp.org 1.nl.pool.ntp.org" /update
+echo ""
+echo "Resyncing clock..."
+w32tm /resync
+echo ""
+echo "Current time source:"
+w32tm /query /source
+echo ""
+echo "All configured time sources:"
+w32tm /query /peers
